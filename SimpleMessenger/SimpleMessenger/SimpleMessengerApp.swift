95291c71c4e53a0f72e95e151e7d9a2a81096e1a
Moein Barzegaran <barzegaranmoein@gmail.com>
2022-08-04 20:41:06 +0430

    Initial commit

---
+++ SimpleMessenger/SimpleMessenger/SimpleMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMessengerApp.swift
+//  SimpleMessenger
+//
+//  Created by Moein Barzegaran on 8/4/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
